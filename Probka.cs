c0cf58830f484795e88b013996be2e641192a68a
dominikchorzepa <dominikchorzepa@wp.pl>
2025-05-27 09:30:30 +0200

    Dodanie klasy Probka i bazy iris

---
+++ Probka.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Knn_algorithm
+{
+    internal class Probka
+    {
+        public double[] Wartosci;
+        public int Klasa;
+
+        public Probka(double[] wartosci, int klasa)
+        {
+            Wartosci = wartosci;
+            Klasa = klasa;
+        }
+    }
+}
